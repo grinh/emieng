30f6f46527a812a49c29a54aeb9c6e1201b2e38e
grinh <grinh@users.noreply.github.com>
2026-05-24 17:20:34 +0000

    Deploy EmiEng c5ae00b5c3b967a66079f8fdddbd3354355443fd grinh/emieng_code@c5ae00b5c3b967a66079f8fdddbd3354355443fd

--- js/storage.js
+++ js/storage.js
@@ -70,3 +70,15 @@ window.emiEngStorage = {
         });
     }
 };
+
+window.emiEngDownload = {
+    downloadTextFile: function (fileName, content) {
+        const blob = new Blob([content], { type: 'text/csv;charset=utf-8' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = fileName;
+        link.click();
+        URL.revokeObjectURL(url);
+    }
+};
